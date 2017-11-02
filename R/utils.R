c9e3b9aaa87161b1019648d23cfe5969b8941c96
brazadas <anaherand@hotmail.com>
2017-11-02 08:41:45 -0500

    adding to table Edit row filters, cleaners

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,8 @@
 is.reactive <- function(obj){
   all(class(obj) %in% c("reactiveExpr","reactive"))
 }
+
+#' @export
+getUrlParameters <- function(session = session) {
+  parseQueryString(session$clientData$url_search)
+}
