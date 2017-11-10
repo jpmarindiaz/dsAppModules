e7952a4afe1671b219209f2361ad95968028e653
jpmarindiaz <jpmarindiaz@gmail.com>
2017-11-10 00:22:26 -0500

    added download button indicators

--- R/downloadHtmlwidget.R
+++ R/downloadHtmlwidget.R
@@ -1,23 +1,9 @@
 #' @export
 downloadHtmlwidgetUI <- function(id, text = "Download", class = NULL){
+  # indicators taken from https://github.com/daattali/advanced-shiny#proxy-click
   ns <- NS(id)
-  appCSS <- "
-  .btn-loading-container {
-    margin-left: 10px;
-    font-size: 1.2em;
-  }
-  .btn-done-indicator {
-  color: green;
-  }
-  .btn-err {
-  margin-top: 10px;
-  color: red;
-  }
-  "
   loadingGif <- "data:image/gif;base64,R0lGODlhEAALAPQAAP///wAAANra2tDQ0Orq6gYGBgAAAC4uLoKCgmBgYLq6uiIiIkpKSoqKimRkZL6+viYmJgQEBE5OTubm5tjY2PT09Dg4ONzc3PLy8ra2tqCgoMrKyu7u7gAAAAAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCwAAACwAAAAAEAALAAAFLSAgjmRpnqSgCuLKAq5AEIM4zDVw03ve27ifDgfkEYe04kDIDC5zrtYKRa2WQgAh+QQJCwAAACwAAAAAEAALAAAFJGBhGAVgnqhpHIeRvsDawqns0qeN5+y967tYLyicBYE7EYkYAgAh+QQJCwAAACwAAAAAEAALAAAFNiAgjothLOOIJAkiGgxjpGKiKMkbz7SN6zIawJcDwIK9W/HISxGBzdHTuBNOmcJVCyoUlk7CEAAh+QQJCwAAACwAAAAAEAALAAAFNSAgjqQIRRFUAo3jNGIkSdHqPI8Tz3V55zuaDacDyIQ+YrBH+hWPzJFzOQQaeavWi7oqnVIhACH5BAkLAAAALAAAAAAQAAsAAAUyICCOZGme1rJY5kRRk7hI0mJSVUXJtF3iOl7tltsBZsNfUegjAY3I5sgFY55KqdX1GgIAIfkECQsAAAAsAAAAABAACwAABTcgII5kaZ4kcV2EqLJipmnZhWGXaOOitm2aXQ4g7P2Ct2ER4AMul00kj5g0Al8tADY2y6C+4FIIACH5BAkLAAAALAAAAAAQAAsAAAUvICCOZGme5ERRk6iy7qpyHCVStA3gNa/7txxwlwv2isSacYUc+l4tADQGQ1mvpBAAIfkECQsAAAAsAAAAABAACwAABS8gII5kaZ7kRFGTqLLuqnIcJVK0DeA1r/u3HHCXC/aKxJpxhRz6Xi0ANAZDWa+kEAA7AAAAAAAAAAAA"
-
   tagList(
-    tags$style(appCSS),
     div(
       `data-for-btn` = ns("downloadHtmlwidget"),
       downloadButton(ns("downloadHtmlwidget"),text, class = class),
